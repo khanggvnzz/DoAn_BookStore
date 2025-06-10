f87ca084cd826761b066129d355c017fcab9d7c9
khanggvnzz <2033221999>
2025-06-10 09:07:33 +0700

    10

--- view/auth/login.php
+++ view/auth/login.php
@@ -132,7 +132,7 @@ if (!isset($_SESSION['user_id']) && isset($_COOKIE['remember_user'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="view/auth/login.css">
+    <link rel="stylesheet" href="/view/auth/login.css">
 </head>
 
 <body>
